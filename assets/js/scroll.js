815d4145b0761775de7763925e174325aecfd996
Pradeep Kumar <pradeepkumar.1729@gmail.com>
2026-08-04 01:04:15 +0530

    added whatsapp floting icon and top nav button

---
+++ assets/js/scroll.js
@@ -0,0 +1,29 @@
+// Scroll To Top Button
+
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+
+        scrollBtn.style.display = "block";
+
+    } else {
+
+        scrollBtn.style.display = "none";
+
+    }
+
+});
+
+scrollBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
